7cdd0edb41a3242f13b83bb50bb9a56c59aade3d
panyao <sunny17178@gmail.com>
2017-09-06 11:59:55 +0800

    版本资源

--- app-api/src/main/java/com/app/client_app/controller/Resource.java
+++ app-api/src/main/java/com/app/client_app/controller/Resource.java
@@ -1,7 +1,24 @@
 package com.app.client_app.controller;
 
+import com.framework.web.ResponseBodyMessage;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 /**
  * Created by @panyao on 2017/8/31.
  */
+@Controller
+@RequestMapping("/api/client/app")
 public class Resource {
+
+    @RequestMapping("/resource")
+    @ResponseBody
+    public ResponseBodyMessage<String> list(@RequestHeader("cli-version") String version) {
+        // 根据版本， 找到对应版本应该更新的资源列表
+
+        return null;
+    }
+
 }
