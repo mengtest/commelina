b5931ff2d2d945ae3da2f3ab63fb9001dfaed0b9
panyao <sunny17178@gmail.com>
2017-09-08 12:26:38 +0800

    配置 docker file

--- bin-build.sh
+++ bin-build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env
 sudo `pwd`/mvnw package
 
-sudo java -jar `pwd`/app-api/target/app-api.jar
-sudo java -jar `pwd`/game-matching/target/game-matching.jar
-sudo java -jar `pwd`/game-room/target/game-room.jar
-sudo java -jar `pwd`/game-gateway/target/game-gateway.jar
+sudo java -jar `pwd`/app-api/target/app-api-1.0.0.jar
+sudo java -jar `pwd`/game-matching/target/game-matching-1.0.0.jar
+sudo java -jar `pwd`/game-room/target/game-room-1.0.0.jar
+sudo java -jar `pwd`/game-gateway/target/game-gateway-1.0.0.jar
