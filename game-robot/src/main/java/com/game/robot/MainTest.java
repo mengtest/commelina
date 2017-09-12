68f1f0fa00408e8480f06638cdf3516506e0d4f4
panYao <sunny17178@163.com>
2017-09-12 22:20:45 +0800

    循环这里的问题

---
+++ game-robot/src/main/java/com/game/robot/MainTest.java
@@ -0,0 +1,19 @@
+package com.game.robot;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by panyao on 2017/9/12.
+ */
+public class MainTest {
+    public static void main(String[] args) {
+        List list = new LinkedList();
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.printf("i" + i);
+        }
+        iii
+                ／／ 明天改一下循环这里的问题
+    }
+}
