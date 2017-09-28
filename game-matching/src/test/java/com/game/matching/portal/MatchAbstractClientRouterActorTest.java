2a211b753d518ffd0788895a0e5be3dbf269f4e2
panyao <sunny17178@gmail.com>
2017-09-28 19:18:41 +0800

    增加cpu监控 listener

--- game-matching/src/test/java/com/game/matching/portal/MatchAbstractClientRouterActorTest.java
+++ game-matching/src/test/java/com/game/matching/portal/MatchAbstractClientRouterActorTest.java
@@ -1,12 +1,8 @@
 package com.game.matching.portal;
 
-import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.testkit.javadsl.TestKit;
 import com.game.matching.MatchingConfigEntity;
-import com.game.matching.proto.MATCHING_METHODS;
-import com.framework.message.ApiRequest;
-import com.framework.message.RequestArg;
 import org.junit.Test;
 
 /**
@@ -21,10 +17,10 @@ public class MatchAbstractClientRouterActorTest {
         MatchingConfigEntity configEntity = new MatchingConfigEntity();
         configEntity.setQueueSuccessPeople(10);
         configEntity.setQueueSizeRate(2);
-        ActorRef actorRef = system.actorOf(MatchingReceiveRequestActor.props(configEntity));
-        actorRef.tell(ApiRequest.newRequest(MATCHING_METHODS.JOIN_MATCH_QUENE, "1.0.0", new RequestArg[]{
-                new RequestArg("1", RequestArg.DATA_TYPE.LONG),
-        }), probe.getRef());
+//        ActorRef actorRef = system.actorOf(MatchingReceiveRequestActor.props(configEntity));
+//        actorRef.tell(ApiRequest.newRequest(MATCHING_METHODS.JOIN_MATCH_QUENE, "1.0.0", new RequestArg[]{
+//                new RequestArg("1", RequestArg.DATA_TYPE.LONG),
+//        }), probe.getRef());
 
 //        MessageBus response = probe.expectMsgClass(MessageBus.class);
 //        assertEquals(OpCodeConstants.JOIN_SUCCESS_RESPONSE, response.getMessage().getOpCode());
