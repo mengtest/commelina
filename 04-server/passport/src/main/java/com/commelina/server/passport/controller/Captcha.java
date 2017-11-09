0e21a5e376a81b8e202d0204701949116f5d4e64
panyao <sunny17178@gmail.com>
2017-11-09 11:25:27 +0800

    重修构建 maven 配置

--- 04-server/passport/src/main/java/com/commelina/server/passport/controller/Captcha.java
+++ 04-server/passport/src/main/java/com/commelina/server/passport/controller/Captcha.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  *
