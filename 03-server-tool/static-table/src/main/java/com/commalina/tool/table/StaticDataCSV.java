cb6e79a177f58581675a04cf745edd616c3a59d9
panyao <sunny17178@gmail.com>
2017-11-21 14:19:58 +0800

    静态表

---
+++ 03-server-tool/static-table/src/main/java/com/commalina/tool/table/StaticDataCSV.java
@@ -0,0 +1,15 @@
+package com.commalina.tool.table;
+
+import java.util.List;
+
+/**
+ * @author panyao
+ * @date 2017/11/21
+ */
+public class StaticDataCSV {
+
+    public <T> List<T> export(String csvDir, Class<T> entity) {
+        return null;
+    }
+
+}
